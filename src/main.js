872c7cb86a371f123f39554ff79c207abb8c4e7a
Myliuquan <775375987@qq.com>
2020-12-12 17:58:57 +0800

    my

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//引入图标样式
+// import './styles/icon.less'
+//动态设置REM基准值
+import 'amfe-flexible'
+//导入vant所有组件
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
+//引入全局样式
+import './styles/index.less'
+
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
