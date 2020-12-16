50c75a6ab86a9bdf027a82c776b218553142861f
Myliuquan <775375987@qq.com>
2020-12-16 20:44:17 +0800

    搜索

--- src/main.js
+++ src/main.js
@@ -13,6 +13,9 @@ import 'vant/lib/index.css';
 //引入全局样式
 import './styles/index.less'
 
+//加载dayjs初始化配置
+import './utils/dayjs'
+
 Vue.use(Vant);
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -35,6 +35,10 @@ const routes = [{
       component: () => import('@/views/my')
     }
   ]
+  }, {
+    path: '/search',
+    name: 'search',
+    component:()=>import('@/views/search')
 }
 ]
 
